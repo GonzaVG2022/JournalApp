39b59e35f1b36a611a81d95179e15ddd42d58d25
Flavio Gonzalo Villalba Gargantini <gonzagargantini20@gmail.com>
2024-01-04 23:32:20 -0300

    redux

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom/client'
 import { JournalApp } from './JournalApp.jsx'
 import './style.css'
 import { BrowserRouter, HashRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import { store } from './store'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <JournalApp/>
-    </BrowserRouter>
+    <Provider store={ store }>
+      <BrowserRouter>
+        <JournalApp/>
+      </BrowserRouter>
+    </Provider>
+    
   </React.StrictMode>,
 )
